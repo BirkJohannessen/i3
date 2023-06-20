a81f7288c8ae95bf5025683a5ab27673254c3d22
Birk Johannessen <birk.johannessen@hotmail.com>
2023-06-20 10:55:57 +0200

    more profile settings

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#birk file: setup monitor on startup
-xrandr --output DP-1 --auto --right-of DP-3
-xrandr --output HDMI-0 --auto --left-of DP-3
-exec nitrogen --restore
-
-#screen save afer 20 mins.
-xset dpms 0 0 1200
