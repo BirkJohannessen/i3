8cadb2416116a3542b600fefe0ec39e7736a457b
Birk Johannessen <birk@machina.no>
2023-07-12 09:37:32 +0200

    delta changes

--- profiles/delta/keyboardStartupFix.sh
+++ profiles/delta/keyboardStartupFix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FNFILE = "/sys/module/hid_apple/parameters/fnmode"
+FNFILE="/sys/module/hid_apple/parameters/fnmode"
 if [[ -f $FNFILE ]]; then
 	sudo sh -c "echo "2" > /sys/module/hid_apple/parameters/fnmode"
 	update-initramfs -u -k all
